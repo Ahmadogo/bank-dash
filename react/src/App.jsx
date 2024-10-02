7cad14e4dde2e906c5187e6a837d96e821a183a3
ahmad <abdulkareemahmad413@gmail.com>
2024-10-02 19:33:17 +0100

    my first commit

---
+++ react/src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element = {<></>} />
+          <Route path="/" element = {<></>} />
+          <Route path="/" element = {<></>} />
+          <Route path="/" element = {<></>} />
+          <Route path="/" element = {<></>} />
+          <Route path="/" element = {<></>} />
+          <Route path="/" element = {<></>} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
